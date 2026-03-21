40e0029b3b7d70b40180de4045ebe0e4dde2223b
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-03-22 00:06:13 +0530

    validanagram

--- validanagram.c
+++ validanagram.c
@@ -5,11 +5,31 @@
 bool isAnagram(char* s, char* t) {
     for(int i=0; i<strlen(s) ; i++){
         for (int j=0; j<strlen(t); j++){
-            if (s[i]!=t[j]){
+            if(s[i]<s[i+1]){
+                char* p[1000] = s[i];
+                s[i]= s[i+1];
+                s[i+1]=p;
+            }
+
+             if(t[i]<t[i+1]){
+                char* q[1000] = t[i];
+                t[i]= t[i+1];
+                t[i+1]=q;
+            }
+
+            if(s[i]==t[j]){
                 continue;
             }
+            else{
+                return false;
+            }
+
+
+
+          
+            }
         }
+        return true;
     }
  
     
-}
