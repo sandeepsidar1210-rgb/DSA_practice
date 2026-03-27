1b0881f658630527db50802fa3a07728fe19ee42
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-03-28 01:54:20 +0530

    wordpattern

---
+++ wordpattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool wordPattern(char* pattern, char* s) {
+    for(int i=0; pattern[i]!='\0';i++){
+        
+    }
+
+    
+}
+
+
+int main(){
+
+}
