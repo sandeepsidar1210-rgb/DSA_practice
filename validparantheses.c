3e6ae4c1179d80a5894a092abb4e87af5681ab8d
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-21 00:00:27 +0530

    validparantheses

---
+++ validparantheses.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<stdbool.h>
+#include <string.h>
+
+bool isValid(char* s) {
+    int n = strlen(s);
+
+    char stack[n];
+    int top = -1;
+
+    for(int i=0; i<n ; i++){
+        char ch = s[i];
+
+        if(ch == '(' || ch == '{' || ch == '['){
+            stack[++top] = ch;
+        }
+        else{
+            if(top == -1){
+                return false;   
+            }
+            else{
+                char popped = stack[top--];
+
+                if((ch == ')' && popped != '(')||
+                (ch == '}'&& popped != '{')||
+                (ch == ']' && popped != '[')
+            ){
+                return false;
+            }
+            }
+        }
+    }
+    return top == -1;
+}
