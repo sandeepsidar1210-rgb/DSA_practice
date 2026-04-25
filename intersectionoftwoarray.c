f65b4a7907dcaf55f7216a737c9e1943f0c8e056
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-04-25 13:20:10 +0530

    intersectionoftwoarray

---
+++ intersectionoftwoarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int new_arr[10001];
+    int index = 0;
+    for(int i=0 ;i < nums1Size; i++){
+        for(int j=0 ; j< nums2Size; j++){
+            if(nums1[i] == nums2[j]){
+                new_arr[index++] = nums2[j];
+                
+            }
+        }
+    }
+}
