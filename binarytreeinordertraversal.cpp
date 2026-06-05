1efff343b17a71c0eb96354222514c25bd8f5eaa
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-06-05 14:06:23 +0530

    binarytreeinordertraversal

---
+++ binarytreeinordertraversal.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+
+ void inorder(TreeNode* root , vector <int> &ans){
+    if(root == NULL){
+        return;
+    }
+    else{
+        inorder(root->left , ans);
+        ans.push_back(root->val);
+        inorder(root->right , ans);
+    }
+ }
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector <int> ans;
+        inorder(root , ans);
+        return ans;
+    }
+};
