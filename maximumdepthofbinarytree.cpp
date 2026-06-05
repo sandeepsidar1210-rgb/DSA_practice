9329930e0200348703a6178a86ed7b24d50b1432
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-06-05 14:38:34 +0530

    maximumdepthofbinarytree

---
+++ maximumdepthofbinarytree.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if( root == NULL){
+            return 0 ;
+        }
+        else{
+            int leftDepth = maxDepth(root -> left);
+            int rightDepth = maxDepth(root -> right);
+            return 1 + max(leftDepth , rightDepth);
+
+        }
+        
+    }
+};
+
+int main(){
+
+}
