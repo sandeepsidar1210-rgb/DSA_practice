b91d979d629e788e01c0ca0cc6c8c6579d73aab3
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-06-11 21:23:31 +0530

    implement stack using queue

---
+++ implementstacksfromqueue.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class MyStack {
+public:
+
+    queue <int> q;
+    int count = 0;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+
+        int sze = q.size();
+        for(int i = 0 ; i< sze -1 ; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        
+
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+        
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty( );
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+ int main() {
+    MyStack st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+
+    cout << st.top() << "\n"; // 3
+
+    cout << st.pop() << "\n"; // 3
+    cout << st.pop() << "\n"; // 2
+    cout << st.pop() << "\n"; // 1
+}
