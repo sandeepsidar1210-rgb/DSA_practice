66911d56f92017a38eb9025a38492ecc60134c4d
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-06-11 20:41:37 +0530

    longestsubstringwithoutrepeatingcharacters

---
+++ longestsubstringwithoutrepeatingcharacters.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<int> st;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0 ; right < s.length(); right++ ){
+           
+            while(st.count(s[right]))
+            {   
+                st.erase(s[left]);
+                left++;
+            }
+
+            st.insert(s[right]);
+
+            ans = max(ans, right - left + 1);
+            
+        }
+    return ans;
+    }
+};
