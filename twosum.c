ed40e371d29cacfefd9616fcf3c1a948917034af
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-06-13 17:01:13 +0530

    two sum in cpp

--- twosum.c
+++ twosum.c
@@ -50,3 +50,27 @@ int main(){
 //         return NULL;
       
 // }
+
+
+//in cpp
+
+/*class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int need = target - nums[i];
+
+            if(mp.find(need) != mp.end()) {
+                return {mp[need], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};*/
