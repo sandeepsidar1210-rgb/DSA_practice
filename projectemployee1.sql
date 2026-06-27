91c3db74331558f6d9cceba901152bae5dc3b085
Sandeep Kumar Sidar <sandeepsidar1210@gmail.com>
2026-06-28 01:49:51 +0530

    project emplyee 1 (SQL)

---
+++ projectemployee1.sql
@@ -0,0 +1,8 @@
+Select p.project_id ,
+Round(AVG(e.experience_years),2) as average_years
+from Project p join Employee e
+on p.employee_id = e.employee_id
+group by p.project_id;
+
+
+-- Round():  Rounds the answer to 2 decimal places.
